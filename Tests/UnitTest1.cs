37c6de87b5077fd8f416e344d48e7f4c8154bf40
Sebastian Petersen <sebastian@srmail.dk>
2018-09-16 17:54:04 +0200

    init

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BilletLibrary;
+using ØresundbroenLibrary;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ ØresundbroenLibrary/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using BilletLibrary;
+
+namespace ØresundbroenLibrary
+{
+    public class Class1
+    {
+    }
+}
